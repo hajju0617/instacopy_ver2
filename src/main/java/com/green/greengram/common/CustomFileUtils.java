116aedaf4dc8037d93ddb4486906cd9ff969ba93
hajju0617 <joowon023617@gmail.com>
2024-05-10 18:08:48 +0900

    ver2

--- src/main/java/com/green/greengram/common/CustomFileUtils.java
+++ src/main/java/com/green/greengram/common/CustomFileUtils.java
@@ -9,6 +9,18 @@ import java.io.File;
 import java.util.UUID;
 
 @Component      // 빈등록
+
+/*
+@Component는 Spring 프레임워크에서 사용되는 어노테이션 중 하나 (클래스를 Spring Bean 으로 등록하기 위해 사용 됨)
+Spring Bean은 Spring 프레임워크에서 관리되는 객체로, 애플리케이션의 구성 요소로 사용 됨
+@Component가 붙은 클래스는 Spring 컨테이너에 의해 자동으로 등록되며, 이후에는 다른 빈 객체와 상호작용하거나, Spring의 서비스를 이용할 수 있음.
+
+1. 자동 등록: @Component가 붙은 클래스는 Spring 컨테이너에 의해 자동으로 등록됨.
+2. 다양한 타입의 빈 등록 가능: @Component는 클래스뿐만 아니라 인터페이스, enum 등 다양한 타입의 빈을 등록할 수 있음
+3. 빈 이름 지정 가능: @Component 어노테이션에 value 속성을 사용하여 빈의 이름을 지정할 수 있음.
+   지정하지 않을 경우 클래스의 이름을 기반으로 빈의 이름이 자동으로 생성됩니다.
+4. 다른 어노테이션과 함께 사용 가능: @Component는 다른 어노테이션과 함께 사용할 수 있습니다
+ */
 @Getter
 public class CustomFileUtils {
     @Value("${file.directory}")
